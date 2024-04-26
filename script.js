2fb3523f545f7a9a36e2f972b8e79d4bef5ceefe
Valentin-KV <krivenko.vg@yandex.ru>
2024-04-26 12:15:00 +0300

    feat: task labels cycles

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let num = 50;
+// let num = 50;
 
 // while (num < 55) {
 //     console.log(num);
@@ -21,10 +21,65 @@ let num = 50;
 // }
 // создаем i (итэрация) и присваиваем ему начальное значение, обычно 1. выполнять цикл пока не выполнится условия для i. Изменяем значение i каждую итэрацию
 
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;  прерывает цикл при достижении условия
-        continue; // продолжает цикл при достижении условия, но исклучает итэрацию условия из вывода
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;  прерывает цикл при достижении условия
+//         continue; // продолжает цикл при достижении условия, но исклучает итэрацию условия из вывода
+//     }
+//     console.log(i);
+// }
+
+// Доп урок по циклам - вложенные циклы
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(j);
+//     }
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+// let result = '';
+// const length = 7;
+
+// for (let i = 1; i < length; i++) {
+    
+//     for (let j = 0; j < i; j++) {
+//         result += "*";
+//     }
+
+
+//     result += '\n';
+// }
+
+// console.log(result);
+
+// Использование меток во вложенных циклах
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) continue first;
+            console.log(`Third level: ${k}`);
+        }
     }
-    console.log(i);
+}
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) break first;
+            console.log(`Third level: ${k}`);
+        }
+    }
 }
